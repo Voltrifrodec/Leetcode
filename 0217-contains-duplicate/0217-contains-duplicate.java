a21a5bb710138d47362cc1f616eab4b76abfb4f9
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-06-03 13:18:30 +0200

    Time: 21 ms (30.70%), Space: 57.9 MB (23.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> hashset = new HashSet<Integer>();
-        for(int i = 0; i < nums.length; i++) {
-            if(hashset.contains(nums[i]))
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++)
+            if(nums[i] == nums[i + 1])
                 return true;
-            hashset.add(nums[i]);
-        }
         return false;
     }
 }
