db005c6a71dddc87bae8bb25a4c277d945f8f989
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-06-05 07:45:10 +0200

    Time: 205 ms (21.38%), Space: 45 MB (5.90%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String resString = s.toLowerCase().replaceAll("\\s", "").replaceAll("[^a-zA-Z0-9]+", "");
+        System.out.println("Replaced string: " + resString);
+        char[] tempString = resString.toCharArray();
+        int j = 0;
+        for(int i = tempString.length - 1; i > 0; i--) {
+            System.out.println("Matching " + tempString[j] + " and " + tempString[i]);
+            if(tempString[j] != tempString[i])
+                return false;
+            j++;
+        }
+        
+        return true;
+    }
+}
