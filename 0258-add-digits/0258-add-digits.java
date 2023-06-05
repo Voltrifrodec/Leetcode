8a9e750cc337fa8df77c16d68e8eec81709592eb
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-06-05 08:41:54 +0200

    Time: 1 ms (100.00%), Space: 40 MB (55.73%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,18 +1,19 @@
 class Solution {
     public int addDigits(int num) {
-        char[] arr = Integer.toString(num).toCharArray();
-        if(arr.length == 1)
-            return num;
-        int sum = 0;
-        while(arr.length != 1) {
-            sum = 0;
-            for(char c : arr) {
-                sum += Character.getNumericValue(c);
-            }
-            arr = Integer.toString(sum).toCharArray();
-            System.out.println(arr.length + " " + sum);
-        }
+        return num == 0 ? 0 : num % 9 == 0 ? 9 : num % 9;
+//         char[] arr = Integer.toString(num).toCharArray();
+//         if(arr.length == 1)
+//             return num;
+//         int sum = 0;
+//         while(arr.length != 1) {
+//             sum = 0;
+//             for(char c : arr) {
+//                 sum += Character.getNumericValue(c);
+//             }
+//             arr = Integer.toString(sum).toCharArray();
+//             System.out.println(arr.length + " " + sum);
+//         }
         
-        return sum;
+//         return sum;
     }
 }
