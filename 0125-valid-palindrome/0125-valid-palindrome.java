25f959470bc8c8f70f12760d93ee4a999d62ed94
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-06-05 07:48:11 +0200

    Time: 26 ms (32.67%), Space: 44.3 MB (9.70%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String resString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]+", "");
-        char[] tempString = resString.toCharArray();
+        char[] resString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]+", "").toCharArray();
         int j = 0;
-        for(int i = tempString.length - 1; i > 0; i--) {
-            if(tempString[j] != tempString[i])
+        for(int i = resString.length - 1; i > 0; i--) {
+            if(resString[j] != resString[i])
                 return false;
             j++;
         }
