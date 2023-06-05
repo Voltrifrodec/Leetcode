2050cbd4675410d1317111ff95b92c05cd82d295
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-06-05 08:42:59 +0200

    Time: 1 ms (100.00%), Space: 40.2 MB (30.56%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -3,20 +3,5 @@ class Solution {
         // Based from the Solution section:
         return num == 0 ? 0 : (num % 9 == 0 ? 9 : num % 9);
         
-        // My previous solution:
-//         char[] arr = Integer.toString(num).toCharArray();
-//         if(arr.length == 1)
-//             return num;
-//         int sum = 0;
-//         while(arr.length != 1) {
-//             sum = 0;
-//             for(char c : arr) {
-//                 sum += Character.getNumericValue(c);
-//             }
-//             arr = Integer.toString(sum).toCharArray();
-//             System.out.println(arr.length + " " + sum);
-//         }
-        
-//         return sum;
     }
 }
