6a72ad33de4babd80ce6ec7289aecbde5cb91ddd
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-06-05 07:50:38 +0200

    Time: 27 ms (29.12%), Space: 43.9 MB (22.00%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,7 +3,7 @@ class Solution {
         char[] resString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]+", "").toCharArray();
         int j = 0;
         for(int i = resString.length - 1; i > 0; i--) {
-            if(resString[j] != resString[i])
+            if(resString[(resString.length - i - 1)] != resString[i])
                 return false;
             j++;
         }
