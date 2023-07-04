b89387fc59f5bcc845b35a3ee887d96b0556b425
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-07-04 10:51:45 +0200

    Time: 1 ms (100.00%), Space: 44.6 MB (43.28%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
-        int match = 0;
-        for(int num : nums)
-            match ^= num;
+        int match = 0;          // Additional variable that will be used for XOR
         
-        return match;
+        for(int num : nums)     // For loop - XOR with all of the elements in array. 
+            match ^= num;       // XOR (Exclusive OR) returns 1 only when both of the elements are different; otherwise 0.
+            
+        
+        return match;           // Return the final element
     }
 }
