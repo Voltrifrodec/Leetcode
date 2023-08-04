637ce1396a38394c5ddd8792242f070514480605
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-08-04 12:34:00 +0200

    Time: 7 ms (90.83%), Space: 42.7 MB (88.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        return Integer.toString(x).equals(new StringBuilder(Integer.toString(x)).reverse().toString());
+        String string = Integer.toString(x);
+        return string.equals(new StringBuilder(string).reverse().toString());
     }
 }
