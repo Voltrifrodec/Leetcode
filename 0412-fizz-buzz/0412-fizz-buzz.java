48b965800ba0abfcb06aadfdb94aa27367a2803e
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-08-11 09:50:12 +0200

    Time: 1 ms (99.62%), Space: 45.1 MB (15.00%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,16 +1,13 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> result = new ArrayList<String>();
+        String string;
         for(int i = 1; i <= n; i++) {
-            if(i % 3 == 0 && i % 5 == 0)
-                result.add("FizzBuzz");
-            else if(i % 3 == 0)
-                result.add("Fizz");
-            else if(i % 5 == 0)
-                result.add("Buzz");
-            else
-                result.add(Integer.toString(i));
+            string =  (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : Integer.toString(i)));
+            result.add(string);
+            // System.out.print(result.get(i - 1) + " ");
         }
+        // System.out.println();
         return result;
     }
 }
