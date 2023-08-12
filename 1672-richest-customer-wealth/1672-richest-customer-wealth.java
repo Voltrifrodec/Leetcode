df070a50ad0f7754052c10328bd448cd94653858
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-08-12 21:29:00 +0200

    Time: 1 ms (9.02%), Space: 41.6 MB (86.20%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int result = 0, temp = 0;
+        for(int i = 0; i < accounts.length; i++) {
+            temp = 0;
+            for(int num: accounts[i]) {
+                temp += num;
+            }
+            result = result < temp ? temp : result;
+        }
+
+        System.out.println(result);
+        return result;
+    }
+}
