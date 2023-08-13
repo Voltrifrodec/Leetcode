e55103b895be09ee95d2a37ae36f348cdbde6dc0
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-08-13 23:24:19 +0200

    Time: 0 ms (100.00%), Space: 39 MB (89.31%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,12 +1,10 @@
 class Solution {
     public int numberOfSteps(int num) {
-
         short steps = 0;
         while(num != 0) {
             num = (num % 2 == 0) ? num / 2 : num - 1;
             steps++;
         }
-
         return steps;
     }
 }
