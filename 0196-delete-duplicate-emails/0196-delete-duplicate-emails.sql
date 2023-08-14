8bd03d75b89d0a50e2e9a403bc3f15e2da1c4136
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-08-14 09:05:29 +0200

    Time: 1659 ms (55.22%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,2 @@
-# Write your MySQL query statement below
-# Doing penetrance between Person (person1) and its clone (person2) using join by email and keeping first that matches the equation (?)
-# if person1.id < person2.id, I'd get the exact opposite -- the greater ID would remain, and smaller excluded (same thing would be achieved by deleting person2 instead of person1)
 DELETE person1 FROM Person person1
 INNER JOIN Person person2 ON person1.email = person2.email AND person1.id > person2.id;
