d7f14e99bc3d4c24446eaa0fffd5ab195bb0e283
Jimmy V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-10-03 10:19:53 +0200

    Time: 1 ms (86.48%), Space: 39.7 MB (73.19%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -5,10 +5,8 @@ class Solution {
 
         for(int i = 0; i < nums.length; i++) {
             for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] == nums[j]) {
-                    System.out.println("Found good pair: (" + i + "," + j + ").");
+                if(nums[i] == nums[j])
                     goodPairs++;
-                }
             }
         }
 
