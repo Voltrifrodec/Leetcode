68ea75027301941be7953fe189fb8b46fa635d45
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-10-05 11:12:45 +0200

    Time: 13 ms (21.73%), Space: 46.2 MB (78.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
 
-        Map<Integer, Integer> uniqueValues = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> uniqueValues = new HashMap<>();
 
         for(int num : nums) {
             if(uniqueValues.containsKey(num)) {
@@ -12,7 +12,7 @@ class Solution {
             }
         }
 
-        List<Integer> values = new ArrayList<Integer>();
+        List<Integer> values = new ArrayList<>();
         for(Map.Entry<Integer, Integer> value: uniqueValues.entrySet()) {
             int valueInt = value.getValue();
             if(valueInt > (nums.length / 3)) {
