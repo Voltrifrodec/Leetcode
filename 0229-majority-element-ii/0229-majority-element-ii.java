754cad95647ee2d5ab881ece44371d3a25ea4295
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-10-05 11:12:16 +0200

    Time: 12 ms (33.23%), Space: 46.3 MB (78.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,7 +4,6 @@ class Solution {
         Map<Integer, Integer> uniqueValues = new HashMap<Integer, Integer>();
 
         for(int num : nums) {
-            System.out.print(num + " ");
             if(uniqueValues.containsKey(num)) {
                 uniqueValues.put(num, uniqueValues.get(num) + 1);
             }
@@ -12,20 +11,14 @@ class Solution {
                 uniqueValues.put(num, 1);
             }
         }
-        System.out.println();
 
         List<Integer> values = new ArrayList<Integer>();
-        // Useful links:
-        //  - https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Map.html#entrySet()
-        //  - https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Map.Entry.html
         for(Map.Entry<Integer, Integer> value: uniqueValues.entrySet()) {
             int valueInt = value.getValue();
             if(valueInt > (nums.length / 3)) {
                 values.add(value.getKey());
-                System.out.print(value.getValue() + "/" + value.getKey() + " ");
             }
         }
-        System.out.println();
 
 
         return values;
