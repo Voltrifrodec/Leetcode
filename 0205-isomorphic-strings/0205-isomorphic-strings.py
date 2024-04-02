ea141da6123b9e133ea6e2300216068f5fbf4b6b
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2024-04-02 23:18:34 +0200

    Time: 48 ms (27.55%), Space: 17.2 MB (20.73%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,9 +2,8 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
         str1 = []
-        str2 = []
-                
         str1.extend(s.index(i) for i in s)
+        str2 = []
         str2.extend(t.index(i) for i in t)
                 
         return True if str1 == str2 else False
