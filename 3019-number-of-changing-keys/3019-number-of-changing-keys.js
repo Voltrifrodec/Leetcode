6a45e9cea75c53845cb7f3809152f21faf487242
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2024-07-18 18:41:39 +0200

    Time: 77 ms (7.28%), Space: 51.9 MB (15.71%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.js
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.js
@@ -7,8 +7,9 @@ var countKeyChanges = function(s) {
     let changes = 0;
     let last_used = s[0].toLowerCase();
     for (let i = 1; i < s.length; i++) {
-        if (s[i].toLowerCase() != last_used) {
-            last_used = s[i].toLowerCase();
+        let c = s[i].toLowerCase();
+        if (c != last_used) {
+            last_used = c;
             changes++;   
         }
     }
