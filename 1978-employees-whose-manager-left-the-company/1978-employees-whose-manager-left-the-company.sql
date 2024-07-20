fb3c373ccb70bb072300f66bfae234173314e45b
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2024-07-20 20:40:40 +0200

    Time: 331 ms (56.35%), Space: 0B (100.00%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
 # Didn't find solution with subquery for now, will use LEFT JOIN for now
-SELECT e1.employee_id
-FROM Employees e1
-LEFT JOIN Employees e2 ON e2.employee_id = e1.manager_id
-WHERE e1.salary < 30000 AND e2.employee_id IS NULL AND e1.manager_id IS NOT NULL
-ORDER BY e1.employee_id;
+SELECT employee_id
+FROM Employees
+WHERE
+    salary < 30000 AND
+    manager_id IS NOT NULL AND
+    manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
+    
